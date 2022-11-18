043e35a335fbcd528ffdb2752733dfe9158109c1
Zewen Wu <z328wu@uwaterloo.ca>
2022-11-18 00:50:34 -0800

    boot up some initial deployment scripts

--- env.sh
+++ env.sh
@@ -4,7 +4,8 @@ set -e
 GIT_HASH=$(git rev-parse --short HEAD)
 export GIT_HASH
 
-BACKEND_ECR_REPO="128958473299.dkr.ecr.us-east-2.amazonaws.com/visio_backend"
-export BACKEND_ECR_REPO
-BACKEND_BUILD_IMAGE_NAME="$BACKEND_ECR_REPO:$GIT_HASH"
-export BACKEND_BUILD_IMAGE_NAME
+VISIO_BACKEND_IMAGE="128958473299.dkr.ecr.us-east-2.amazonaws.com/visio_backend"
+export VISIO_BACKEND_IMAGE
+
+VISIO_AWS_PROFILE='--profile visio-eng'
+export VISIO_AWS_PROFILE
