9df03dd2338988cd07c5d49d6dc4b11527f49dfa
Tristan Le <tristan.tuanle@gmail.com>
2023-01-18 20:39:54 -0500

    Skeleton code for pose detection and depth

--- visio_ml/tutorials/custom_pipeline.py
+++ visio_ml/tutorials/custom_pipeline.py
@@ -24,6 +24,16 @@ spatial_data_out.input.setBlocking(False)
 spatial_calc_config_in = pipeline.createXLinkIn()
 spatial_calc_config_in.setStreamName("spatial_calc_config_in")
 
+top_left = dai.Point2f(0.4, 0.4)
+bottom_right = dai.Point2f(0.6, 0.6)
+config = dai.SpatialLocationCalculatorConfigData()
+config.depthThresholds.lowerThreshold = 100
+config.depthThresholds.upperThreshold = 10000
+config.roi = dai.Rect(top_left, bottom_right)
+
+spatial_location_calculator.inputConfig.setWaitForMessage(False)
+spatial_location_calculator.initialConfig.addROI(config)
+
 spatial_location_calculator.out.link(spatial_data_out.input)
 spatial_calc_config_in.out.link(spatial_location_calculator.inputConfig)
 
@@ -32,12 +42,27 @@ depth_in_frame.setStreamName("depth_in_frame")
 depth_in_frame.out.link(spatial_location_calculator.inputDepth)
 
 with dai.Device(pipeline) as device:
-    qIn = device.getInputQueue(name="depth_in_frame")
+    depth_queue = device.getInputQueue(name="depth_in_frame")
+    spatial_calc_queue = device.getOutputQueue(name="spatial_data_out", maxSize=4, blocking=False)
+    spatial_calc_config_in_queue = device.getInputQueue("spatial_calc_config_in")
     depth_frame = None
 
     with mp_pose.Pose( min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
       while True:
-         # Receive RGB frame and run BlazePose
-         pass
+        # Receive RGB frame and run BlazePose
+         
+        # Receive depth frame and calculate spatial info
+        # TODO: implement receiving depth frame from websocket
+        depth_queue.send(depth_frame)
+
+        spatial_data = spatial_calc_queue.get().getSpatialLocations()
+        # TODO: do something with spatial_data
+
+        config.roi = dai.Rect(top_left, bottom_right)
+        config.calculationAlgorithm = dai.SpatialLocationCalculatorAlgorithm.AVERAGE
+        cfg = dai.SpatialLocationCalculatorConfig
+        cfg.addROI(config)
+        spatial_calc_config_in_queue.send(cfg)
+        pass
       
-      
+         
