ecedd013814f0f3da4fa1413abf7257a9b9c059d
usephysics <rttan@uwaterloo.ca>
2023-02-17 22:43:03 -0500

    base functions

--- visio_ml/src/modules/compile_results.py
+++ visio_ml/src/modules/compile_results.py
@@ -1,12 +1,31 @@
 import cv2
+from collections import defaultdict
 
 class ResultCompiler:
     def __init__(self):
-        self.angles = []
+        self.results = defaultdict(defaultdict(list))
         self.out = cv2.VideoWriter("results.avi", cv2.VideoWriter_fourcc('M','J','P','G'), 10, (1152, 648))
     
-    def record_angle(self, angle):
+    def record_angle(self, angle) -> None:
+        arr = angle.split(":")
+        if len(arr) != 3:
+            raise Exception("Invalid angle input")
+
+        body_part, name, angle = arr[0], arr[1], float(arr[2])
+        self.results[body_part][name].append(angle)
+
+    def record_frame(self, frame) -> None:
+        self.out.write(frame)
+
+    def get_average_range(self, body_part, name) -> float:
+        total = 0
+        for val in self.results[body_part][name]:
+            total += val
+        
+        return total / len(self.results[body_part][name])
+
+    def store_video_in_firebase(self) -> None:
         pass
-    
-    def record_frame(self, frame):
+
+    def store_results_in_firebase(self) -> None:
         pass
