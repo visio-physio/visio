202b471cdc851f5d136ac771456d492658d2f302
usephysics <rttan@uwaterloo.ca>
2023-02-22 15:51:10 -0500

    fixes and add custom file name

--- visio_ml/src/modules/compile_results.py
+++ visio_ml/src/modules/compile_results.py
@@ -1,5 +1,6 @@
 import cv2
 import firebase_admin
+import datetime
 
 class ResultCompiler:
     PATH_TO_CREDENTIALS = "" # TODO: Add file path to firebase credentials
@@ -7,7 +8,7 @@ class ResultCompiler:
     # results_db_path: URL to the realtime database for results
     # results_destination_path: destination file in firebase storage for results
     # video_db_name: name of the realtime database for videos
-    def __init__(self, exercise, body_part, results_db_path, results_destination_path, video_db_name):
+    def __init__(self, exercise, body_part, results_db_path, results_destination_path, video_db_name, frame_rate):
         self.cred = firebase_admin.credentials.Certificate(self.PATH_TO_CREDENTIALS)
         self.exercise = exercise
         self.body_part = body_part
@@ -16,7 +17,8 @@ class ResultCompiler:
         self.video_db_name = video_db_name
         self.results = []
         self.total = 0
-        self.out = cv2.VideoWriter("results.avi", cv2.VideoWriter_fourcc('M','J','P','G'), 10, (1152, 648))
+        self.video_file_name = f"{exercise}_{body_part}_{frame_rate}fps_{datetime.now()}.avi"
+        self.out = cv2.VideoWriter(self.video_file_name, cv2.VideoWriter_fourcc('M','J','P','G'), frame_rate, (1152, 648))
     
     def record_angle(self, angle) -> None:
         self.results.append(angle)
@@ -31,12 +33,12 @@ class ResultCompiler:
     def store_video_in_firebase(self) -> None:
         self.out.release()
         firebase_admin.initialize_app(self.cred, {
-            "databaseURL": self.results_db_path
+            "storageBucket": self.video_db_name
         })
 
         bucket = firebase_admin.storage.bucket()
-        blob = bucket.blob("./results.avi")
-        blob.upload_from_filename("./results.avi")
+        blob = bucket.blob(f"./{self.video_file_name}")
+        blob.upload_from_filename(f"./{self.video_file_name}")
 
     def store_results_in_firebase(self) -> None:
         firebase_admin.initialize_app(self.cred, {
