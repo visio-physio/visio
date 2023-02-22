81b6247345dafdf3d76871568954b410c98960df
usephysics <rttan@uwaterloo.ca>
2023-02-22 15:40:57 -0500

    replace logic

--- visio_ml/src/modules/compile_results.py
+++ visio_ml/src/modules/compile_results.py
@@ -1,42 +1,44 @@
 import cv2
 import firebase_admin
-from collections import defaultdict
 
 class ResultCompiler:
     PATH_TO_CREDENTIALS = "" # TODO: Add file path to firebase credentials
-    PATH_TO_DATABASE = ""    # TODO: Add URL to the realtime database
-    PATH_TO_DESTINATION = "" # TODO: Add destination file in firebase storage
 
-    def __init__(self):
-        self.results = defaultdict(lambda: defaultdict(list))
+    # db_path: URL to the realtime database for results
+    # video_db_name: name of the realtime database for videos
+    # destination_path: destination file in firebase storage
+    def __init__(self, exercise, body_part, db_path, video_db_name, destination_path):
+        self.cred = firebase_admin.credentials.Certificate(self.PATH_TO_CREDENTIALS)
+        self.exercise = exercise
+        self.body_part = body_part
+        self.results_db_path = db_path
+        self.video_db_name = video_db_name
+        self.destination_path = destination_path
+        self.results = []
+        self.total = 0
         self.out = cv2.VideoWriter("results.avi", cv2.VideoWriter_fourcc('M','J','P','G'), 10, (1152, 648))
     
     def record_angle(self, angle) -> None:
-        arr = angle.split(":")
-        if len(arr) != 3:
-            raise Exception("Invalid angle input")
-
-        body_part, name, angle = arr[0], arr[1], float(arr[2])
-        self.results[body_part][name].append(angle)
+        self.results.append(angle)
+        self.total += angle
 
     def record_frame(self, frame) -> None:
         self.out.write(frame)
 
-    def get_average_range(self, body_part, name) -> float:
-        total = 0
-        for val in self.results[body_part][name]:
-            total += val
-        
-        return total / len(self.results[body_part][name])
+    def get_average_range(self) -> float:
+        return self.total / len(self.results)
 
     def store_video_in_firebase(self) -> None:
-        pass
-
-    def store_results_in_firebase(self) -> None:
-        cred = firebase_admin.credentials.Certificate(self.PATH_TO_CREDENTIALS)
-        firebase_admin.initialize_app(cred, {
-            "databaseURL": self.PATH_TO_DATABASE
+        self.out.release()
+        firebase_admin.initialize_app(self.cred, {
+            "databaseURL": self.results_db_path
         })
 
-        ref = firebase_admin.db.reference(self.PATH_TO_DESTINATION)
-        ref.set(self.results)
+    def store_results_in_firebase(self) -> None:
+        firebase_admin.initialize_app(self.cred, {
+            "storageBucket": self.video_db_name
+        })
+
+        bucket = firebase_admin.storage.bucket()
+        blob = bucket.blob("./results.avi")
+        blob.upload_from_filename("./results.avi")
