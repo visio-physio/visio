98b2161826674ec373489ab3fadb6b1c52ba28db
usephysics <rttan@uwaterloo.ca>
2023-02-22 20:22:23 -0500

    account for peaks and valleys

--- visio_ml/src/modules/compile_results.py
+++ visio_ml/src/modules/compile_results.py
@@ -1,6 +1,7 @@
 import cv2
 import firebase_admin
 import datetime
+from collections import defaultdict
 
 class ResultCompiler:
     PATH_TO_CREDENTIALS = "" # TODO: Add file path to firebase credentials
@@ -8,27 +9,39 @@ class ResultCompiler:
     # results_db_path: URL to the realtime database for results
     # results_destination_path: destination file in firebase storage for results
     # video_db_name: name of the realtime database for videos
-    def __init__(self, exercise, body_part, results_db_path, results_destination_path, video_db_name, frame_rate):
+    def __init__(self, user_id, exercise, body_part, results_db_path, results_destination_path, video_db_name, frame_rate, frame_size, local_file_path):
         self.cred = firebase_admin.credentials.Certificate(self.PATH_TO_CREDENTIALS)
         self.exercise = exercise
         self.body_part = body_part
         self.results_db_path = results_db_path
         self.results_destination_path = results_destination_path
         self.video_db_name = video_db_name
-        self.results = []
-        self.total = 0
-        self.video_file_name = f"{exercise}_{body_part}_{frame_rate}fps_{datetime.now()}.avi"
-        self.out = cv2.VideoWriter(self.video_file_name, cv2.VideoWriter_fourcc('M','J','P','G'), frame_rate, (1152, 648))
+        self.results = defaultdict(lambda: [0])
+        self.peaks_and_valleys = defaultdict(list)
+        self.video_file_name = f"{local_file_path}/{user_id}_{exercise}_{body_part}_{frame_rate}fps_{datetime.now()}.avi"
+        self.out = cv2.VideoWriter(self.video_file_name, cv2.VideoWriter_fourcc('M','J','P','G'), frame_rate, frame_size)
     
     def record_angle(self, angle) -> None:
-        self.results.append(angle)
-        self.total += angle
+        for key, val in angle.items():
+            if len(self.results) > 1:
+                prev_delta = self.results[-1] - self.results[-2]
+                curr_delta = val - self.results[-1]
+                if prev_delta * curr_delta < 0:
+                    self.peaks_and_valleys[key].append(self.results[-1])
+
+            self.results[key].append(val)
 
     def record_frame(self, frame) -> None:
         self.out.write(frame)
 
-    def get_average_range(self) -> float:
-        return self.total / len(self.results)
+    # side should be left or right
+    def get_average_range(self, side) -> float:
+        total = 0
+        arr = self.peaks_and_valleys[side]
+        for i in range(1, len(arr)):
+            total += abs(arr[i] - arr[i - 1])
+
+        return total / (len(arr) - 1)
 
     def store_video_in_firebase(self) -> None:
         self.out.release()
