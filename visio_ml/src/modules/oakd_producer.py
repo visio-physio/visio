c6bbee0bb515e66df39c7387d2a5209f080ced5b
Tristan Le <tristan.tuanle@gmail.com>
2023-03-02 23:35:43 -0500

    Bug fix

--- visio_ml/src/modules/oakd_producer.py
+++ visio_ml/src/modules/oakd_producer.py
@@ -19,6 +19,7 @@ class OakdProducer():
         self.date = None
         self.websocket = None
         self.cpu = cpu
+        self.state = ['shoulder', 'hip']
 
     async def serve(self, host, port):
         server = await websockets.serve(self.handler, host, port)
@@ -36,12 +37,24 @@ class OakdProducer():
             # self.date = event['date']
             self.state = message
         print(f"State updated to: {self.state}")
+
+    async def send_frame(self, frame):
+        # Convert the frame to a byte string
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame_bytes = buffer.tobytes()
+        compressed_frame_bytes = gzip.compress(frame_bytes)
+        # Encode the byte string as a base64 string
+        frame_base64 = base64.b64encode(compressed_frame_bytes).decode('utf-8')
+        # Send the base64 string to the client
+        await self.websocket.send(frame_base64)
+
     
     async def produce(self):
         if self.cpu:
             tracker = VisioPose(
                 crop=True,
-                internal_frame_height=600
+                internal_frame_height=600,
+                lm_model='lite'
             )
             renderer = VisioPoseRenderer(tracker)
         else:
@@ -55,14 +68,18 @@ class OakdProducer():
 
         while True:
             await asyncio.sleep(0.001)
-            # exercises
-            states = ['shoulder', 'hip']
-            if self.state in states:
-                frame, body = tracker.next_frame()
-                if frame is None:
-                    break
 
-                if body.landmarks:
+            frame, body = tracker.next_frame()
+            if frame is None:
+                break
+
+            
+            if self.state in self.states:
+                # frame, body = tracker.next_frame()
+                # if frame is None:
+                #     break
+
+                if body.landmarks is not None:
                     # To_do task: reformat the get_measurement para, passed in self.state only
                     '''
                     example: if you want hip abduction use: state = "hip_abduction"
@@ -78,25 +95,27 @@ class OakdProducer():
                         y_coord += 15
                 frame = renderer.draw(frame, body)
 
-                 # Convert the frame to a byte string
-                _, buffer = cv2.imencode('.jpg', frame)
-                frame_bytes = buffer.tobytes()
-                compressed_frame_bytes = gzip.compress(frame_bytes)
-                # Encode the byte string as a base64 string
-                frame_base64 = base64.b64encode(compressed_frame_bytes).decode('utf-8')
-                # Send the base64 string to the client
-                await self.websocket.send(frame_base64)
+                await self.send_frame(frame)
 
-                key = renderer.waitKey(delay=1)
-                if key == ord('q') or key == 27:
-                    break
+                # key = renderer.waitKey(delay=1)
+                # if key == ord('q') or key == 27:
+                #     break
 
                 # send to results compiler based on exercise
+            elif self.state == 'end':
+                # TODO: Result compiler code
+                
+                self.state = 'idle'
+            
+            key = renderer.waitKey(delay=1)
+            if key == ord('q') or key == 27:
+                break
+
         renderer.exit()
         tracker.exit()
 
 if __name__ == "__main__":
-    server = OakdProducer(cpu=False)
+    server = OakdProducer(cpu=True)
     ip = "127.0.0.1"
     print(f"Starting server at {ip}:8080")
     asyncio.run(server.serve(ip, 8080))
