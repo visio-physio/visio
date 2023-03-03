56bed68326d62439e497736eab34ea5cc71ee35f
Zewen Wu <z328wu@uwaterloo.ca>
2023-03-03 11:12:04 -0800

    working server code

--- visio_ml/src/modules/depthai_blazepose/mediapipe_utils.py
+++ visio_ml/src/modules/depthai_blazepose/mediapipe_utils.py
@@ -150,8 +150,8 @@ class Body:
             right_shoulder_angle = self.get_angle(self.project_on_anatomical_plane(right_arm, 'sagittal'), spine)
             left_shoulder_angle = self.get_angle(self.project_on_anatomical_plane(left_arm, 'sagittal'), spine)
 
-        results = {"right_shoulder": right_shoulder_angle,
-                   "left_shoulder": left_shoulder_angle}
+        results = {"right": right_shoulder_angle,
+                   "left": left_shoulder_angle}
         return results
     
     def get_hip_angles(self, exercise):
@@ -168,8 +168,8 @@ class Body:
             right_hip_angle = self.get_angle(self.project_on_anatomical_plane(right_thigh, 'sagittal'), spine)
             left_hip_angle = self.get_angle(self.project_on_anatomical_plane(left_thigh, 'sagittal'), spine)
 
-        results = {"right_hip": right_hip_angle,
-                   "left_hip": left_hip_angle}
+        results = {"right": right_hip_angle,
+                   "left": left_hip_angle}
         return results
 
     def print(self):
