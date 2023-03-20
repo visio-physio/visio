992a805600b9c297c99b84c13fa45a79552f94b7
SyedShayaan <45016383+SyedShayaan@users.noreply.github.com>
2023-03-19 21:50:13 -0400

    added toolbar to navigate between exercises in exercise detail and removed back button

--- visio_frontend/Visio/Viso/Views/Exercises/ExerciseDetail.swift
+++ visio_frontend/Visio/Viso/Views/Exercises/ExerciseDetail.swift
@@ -1,136 +1,10 @@
-//import SwiftUI
-//import Firebase
-//
-//struct ExerciseDetail: View {
-//    @EnvironmentObject var cam: CameraWebsocket
-//    @EnvironmentObject var load_exercises: LoadExercises
-//
-//    @State private var isLiveCameraViewActive = false
-//    @State private var selectedExerciseDetail: Exercise?
-//    @State var exercise: Exercise
-//
-//    let userID = Auth.auth().currentUser?.uid ?? "none"
-//
-//
-//
-//    @State private var isShowVideo = false
-//    var body: some View {
-//        TabView {
-//            NavigationStack {
-//                ScrollView {
-//                    VStack {
-//                        Text("Results")
-//                            .font(.largeTitle)
-//                            .fontWeight(.bold)
-//                            .padding(.top)
-//
-//                        ExerciseRangeOfMotionPlotView()
-//                            .environmentObject(Results(collection: "results", document: userID, exerciseType: exercise.exercise + "-" + exercise.bodyPart))
-//
-//                        HStack {
-//                            CircleImage(image: exercise.image)
-//                                .padding(.top, 20)
-//
-//                            VStack(alignment: .leading) {
-//                                Text(exercise.bodyPart + " " + exercise.exercise)
-//                                    .font(.title)
-//                                    .fontWeight(.bold)
-//                                Text(exercise.measurementField)
-//                                    .font(.headline)
-//                                    .padding(.top, 8)
-//                                Text(exercise.measurementRange)
-//                                    .font(.subheadline)
-//                                    .foregroundColor(.gray)
-//                            }
-//                            .padding(.leading)
-//
-//                            Spacer()
-//
-//                            FavoriteButton(isSet: $exercise.isFavorite, exercise_id: exercise.id)
-//                                .padding(.trailing)
-//                        }
-//
-//                        Divider()
-//                            .padding(.vertical)
-//
-//                        VStack(alignment: .leading, spacing: 16) {
-//                            Text(exercise.descriptionTitle)
-//                                .font(.title2)
-//                                .fontWeight(.bold)
-//
-//                            HStack(alignment: .top) {
-//                                Image(exercise.imageExample)
-//                                    .resizable()
-//                                    .frame(width: 150, height: 150)
-//
-//                                Text(exercise.description)
-//                                    .padding(.leading)
-//                            }
-//
-//
-//                        }
-//                        .padding()
-//
-//                        Spacer()
-//                        Button("Start Test") {
-//                            cam.send(userId: userID, bodyPart: self.exercise.bodyPart, exercise: self.exercise.exercise, state: "start")
-//                            isLiveCameraViewActive = true
-//                            print("sending to server: \(self.exercise.exercise)")
-//                        }
-//                        .buttonStyle(LiveCameraButtonStyle())
-//                        .sheet(isPresented: $isLiveCameraViewActive) {
-//                            LiveCameraView()
-//                        }
-//
-//                    }
-//                }
-//                .padding(.horizontal)
-//                .navigationBarTitle(Text(exercise.bodyPart + " " + exercise.exercise), displayMode: .inline)
-//            }
-//            .tabItem {
-//                Label("Exercise", systemImage: "person.circle")
-//            }
-//
-//            ResultsView(exercise: exercise.bodyPart + " " + exercise.exercise)
-//                .environmentObject(Results(collection: "results", document: userID, exerciseType: exercise.exercise + "-" + exercise.bodyPart))
-//                .tabItem {
-//                    Label("Results", systemImage: "chart.line.uptrend.xyaxis")
-//                }
-//        }
-//    }
-//}
-//
-//struct LiveCameraButtonStyle: ButtonStyle {
-//    func makeBody(configuration: Configuration) -> some View {
-//        configuration.label
-//            .padding(.horizontal, 20)
-//            .padding(.vertical, 10)
-//            .background(Color.green)
-//            .cornerRadius(10)
-//            .foregroundColor(.white)
-//    }
-//}
-//
-//struct ExerciseDetail_Previews: PreviewProvider {
-//    static let load_data = LoadExercises()
-//    static var exercise = load_data.exercises[0]
-//
-//    static var previews: some View {
-//        ExerciseDetail(exercise: exercise)
-//
-//    }
-//
-//}
-//
-//
-
-
 import SwiftUI
 import Firebase
 
 struct ExerciseDetail: View {
     @EnvironmentObject var cam: CameraWebsocket
     @EnvironmentObject var load_exercises: LoadExercises
+    @Environment(\.presentationMode) var presentationMode
 
     @State private var isLiveCameraViewActive = false
     @State  var selectedExercises: [Exercise]
@@ -143,6 +17,35 @@ struct ExerciseDetail: View {
             VStack {
                 ScrollView {
                     VStack {
+                        HStack {
+                            Button(action: {
+                                presentationMode.wrappedValue.dismiss()
+                            }) {
+                                Label("End Exam", systemImage: "xmark.circle")
+                            }
+                            
+                            Spacer()
+                            
+                            Button(action: {
+                                if currentIndex > 0 {
+                                    currentIndex -= 1
+                                }
+                            }) {
+                                Label("Previous", systemImage: "arrow.left")
+                            }
+                            
+                            Spacer()
+
+                            Button(action: {
+                                if currentIndex < selectedExercises.count - 1 {
+                                    currentIndex += 1
+                                }
+                            }) {
+                                Label("Next", systemImage: "arrow.right")
+                            }
+                        }
+                        .padding(.top)
+                        
                         Text("Results")
                             .font(.largeTitle)
                             .fontWeight(.bold)
@@ -203,7 +106,6 @@ struct ExerciseDetail: View {
                         .sheet(isPresented: $isLiveCameraViewActive) {
                             LiveCameraView()
                         }
-
                     }
                 }
                 .padding(.horizontal)
@@ -219,26 +121,7 @@ struct ExerciseDetail: View {
                     Label("Results", systemImage: "chart.line.uptrend.xyaxis")
                 }
         }
-        VStack {
-            HStack {
-                Button("Previous") {
-                    if currentIndex > 0 {
-                        currentIndex -= 1
-                    }
-                }
-                Spacer()
-                Button("Next") {
-                    if currentIndex < selectedExercises.count - 1 {
-                        currentIndex += 1
-                    }
-                }
-                Spacer()
-                Button("End Exam") {
-                    // Handle the end of the exam here
-                }
-            }
-            .padding()
-        }
+        .navigationBarBackButtonHidden(true) // Add this line to hide the back button
     }
 }
 
