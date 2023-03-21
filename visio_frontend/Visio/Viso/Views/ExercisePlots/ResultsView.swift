55a60ef8cf6ce16643410752b288c5f9b78521fd
SyedShayaan <45016383+SyedShayaan@users.noreply.github.com>
2023-03-21 14:27:27 -0400

    added in notes

--- visio_frontend/Visio/Viso/Views/ExercisePlots/ResultsView.swift
+++ visio_frontend/Visio/Viso/Views/ExercisePlots/ResultsView.swift
@@ -1,11 +1,13 @@
 import SwiftUI
 import Charts
+import PencilKit
 
 struct ResultsView: View {
     var exercise: String
     @EnvironmentObject var results: Results
-    
+    @State private var tool: PKTool = PKInkingTool(.pen, color: .black, width: 10)
     @State private var selectedRange = "Left"
+    @State private var canvas = PKCanvasView()
     
     private var initialLeftRange: Double {
         results.datapoints.first?.roi_left ?? 0
@@ -32,43 +34,85 @@ struct ResultsView: View {
     }
 
     var body: some View {
-        VStack {
-            Text("Results for \(exercise.localizedUppercase)")
-                .font(.largeTitle)
+        
+        ScrollView {
+            VStack {
+                Text("Results for \(exercise.localizedUppercase)")
+                    .font(.largeTitle)
+                    .padding()
+                
+                Divider()
+                
+                Picker("Range of Motion", selection: $selectedRange) {
+                    Text("Left").tag("Left")
+                    Text("Right").tag("Right")
+                }
+                .pickerStyle(SegmentedPickerStyle())
                 .padding()
-            
-            Divider()
-            
-            Picker("Range of Motion", selection: $selectedRange) {
-                Text("Left").tag("Left")
-                Text("Right").tag("Right")
-            }
-            .pickerStyle(SegmentedPickerStyle())
-            .padding()
-            
-            MotionGraph(dataPoints: results.datapoints, rangeType: selectedRange)
-            
-            HStack {
+                
+                MotionGraph(dataPoints: results.datapoints, rangeType: selectedRange)
+                
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Summary")
+                        .font(.title2)
+                        .bold()
+                    
+                    HStack {
+                        VStack(alignment: .leading, spacing: 6) {
+                            Text("Latest Left Range of Motion:")
+                            Text("Latest Right Range of Motion:")
+                        }
+                        VStack(alignment: .leading, spacing: 6) {
+                            Text("\(latestLeftRange, specifier: "%.2f")")
+                            Text("\(latestRightRange, specifier: "%.2f")")
+                        }
+                        Spacer()
+                    }
+                    
+                    HStack {
+                        VStack(alignment: .leading, spacing: 6) {
+                            Text("Left Improvement:")
+                            Text("Right Improvement:")
+                        }
+                        VStack(alignment: .leading, spacing: 6) {
+                            Text("\(leftImprovementPercentage, specifier: "%.2f")%")
+                            Text("\(rightImprovementPercentage, specifier: "%.2f")%")
+                        }
+                        Spacer()
+                    }
+                }
+                .padding()
                 
                 VStack(alignment: .leading) {
-                    Text("Latest Left Range of Motion: \(latestLeftRange, specifier: "%.2f")")
-                    Text("Latest Right Range of Motion: \(latestRightRange, specifier: "%.2f")")
+                    Text("Notes")
+                        .font(.title2)
+                        .bold()
+                    
+                    DrawingView(canvas: $canvas)
+                        .frame(height: 300)
+                        .background(Color.white)
+                        .cornerRadius(8)
+                        .shadow(radius: 4)
+                    
                 }
+                .padding()
+
                 Spacer()
             }
-            .padding()
-            
-            // Add a summary section to show the overall improvement percentage
-            // for both Left and Right Range of Motion
-            VStack(alignment: .leading) {
-                Text("Left Improvement: \(leftImprovementPercentage, specifier: "%.2f")%")
-                Text("Right Improvement: \(rightImprovementPercentage, specifier: "%.2f")%")
-            }
-            .padding()
-
-            Spacer()
         }
     }
+}
+
+struct DrawingView: UIViewRepresentable {
+    @Binding var canvas: PKCanvasView
+
+    func makeUIView(context: Context) -> PKCanvasView {
+        canvas.drawingPolicy = .anyInput
+        canvas.tool = PKInkingTool(.pen, color: .black, width: 10)
+        return canvas
+    }
+
+    func updateUIView(_ uiView: PKCanvasView, context: Context) {}
 }
 
 struct MotionGraph: View {
